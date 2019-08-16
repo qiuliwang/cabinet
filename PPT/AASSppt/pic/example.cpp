7746686d1b70dbc1ec402fe85cbf50ee916fa846
wangqiuli0102 <418674878@qq.com>
2019-08-16 19:18:39 +0800

    addn

---
+++ PPT/AASSppt/pic/example.cpp
@@ -0,0 +1,61 @@
+
+if (a >= b) {
+    c = d + b; // Comment1
+    d = d + 1;}
+else
+    c = d - a; //Comment2
+
+if (a>=b) {
+    // Comment1'
+    c=d+b;
+    d=d+1;}
+else // Comment2'
+    c=d-a;
+
+
+
+if (a >= b) {
+    c = d + b; // Comment1
+    d = d + 1;
+}
+else
+    c = d - a; //Comment2
+
+if (m >= n)
+    { // Comment1'
+    y = x + n;
+    x = x + 5; //Comment3
+    }
+else
+    y = x - m; //Comment2'
+
+
+
+
+
+
+if (a >= b) {
+    c = d + b; // Comment1
+    d = d + 1;}
+else
+    c = d - a; //Comment2
+
+
+if (a >= b) {
+    c = d + b; // Comment1
+    e = 1; // This statement is added
+    d = d + 1; }
+else
+    c = d - a; //Comment2
+
+
+
+
+int i, j=1;
+for (i=1; i<=VALUE; i++)
+    j=j*i;
+
+int factorial(int n) {
+if (n == 0) return 1 ;
+else return n * factorial(n-1) ;
+}
